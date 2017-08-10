1b1837971a0af7ffde7c672f3a5775d34c77ffc6
corianna <annam2004@mail.ru>
2017-08-10 17:57:06 -0500

    Uploadind final project

---
+++ Final Project AC/scripts/appjava.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+  $(".carousel").carousel();	
+  $('#one').on('click', function(event) {
+  	window.location.href = 'slovenia.html';
+  });
+
+  $('#two').on("click", function(event) {
+  	window.location.href = "slovenia.html";
+
+  });
+
+  $("#three").on("click", function(event) {
+  	window.location.href = "slovenia.html";
+
+  });
+
+  $("four").on("click", function(event) {
+  	window.location.href = "slovenia.html";
+  })
+
+  $("five").on("click",function(event){
+  	window.location.href = "slovenia.html";
+  	  })
+
+});
+
+
